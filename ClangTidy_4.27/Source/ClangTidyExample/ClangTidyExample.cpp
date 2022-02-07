e593927fbe81fe326415efada4a2112123536424
June Rhodes <jrhodes@redpoint.games>
2022-02-07 16:46:41 +1100

    Merge clang-tidy example into this repository

---
+++ ClangTidy_4.27/Source/ClangTidyExample/ClangTidyExample.cpp
@@ -0,0 +1,6 @@
+// Copyright June Rhodes. MIT Licensed.
+
+#include "ClangTidyExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClangTidyExample, "ClangTidyExample" );
