4ff00a9469b38c9bf52d3ad8894f91170d51309c
June Rhodes <jrhodes@redpoint.games>
2022-02-07 18:55:54 +1100

    Merge in the Game Management Framework example

--- BuildScriptsExtra/EOS_CPlusPlus_4.27_Clone.ps1
+++ BuildScriptsExtra/EOS_CPlusPlus_4.27_Clone.ps1
@@ -1,41 +1,9 @@
 param()
 
-Push-Location $PSScriptRoot\..\EOS_CPlusPlus_4.27
-try {
-    if (Test-Path .\Plugins) {
-        Remove-Item -Force -Recurse .\Plugins
-    }
-    New-Item -ItemType Directory .\Plugins
-    Push-Location .\Plugins
-    try  {
-        if ($env:CI -eq "true") {
-            git clone --recursive "https://gitlab-ci-token:$env:CI_JOB_TOKEN@gitlab.com/redpointgames/eos-online-subsystem.git" EOS
-            if ($LastExitCode -ne 0) {
-                exit $LastExitCode
-            }
-        } else {
-            git clone --recursive "git@gitlab.com:redpointgames/eos-online-subsystem.git" EOS
-            if ($LastExitCode -ne 0) {
-                exit $LastExitCode
-            }
-        }
-        git --git-dir=EOS/.git --work-tree=EOS checkout $(git --git-dir=EOS/.git --work-tree=EOS describe --tags --abbrev=0)
-        if ($LastExitCode -ne 0) {
-            exit $LastExitCode
-        }
-        Push-Location ./EOS
-        try {
-            git submodule update --init --recursive
-            if ($LastExitCode -ne 0) {
-                exit $LastExitCode
-            } 
-        } finally { 
-            Pop-Location 
-        }
-    } finally {
-        Pop-Location
-    }
-} finally {
-    Pop-Location
-}
+. $PSScriptRoot\CloneHelper.ps1
+
+Set-Location $PSScriptRoot\..\EOS_CPlusPlus_4.27
+
+Clone-Repository "eos-online-subsystem" "EOS"
+
 exit 0
