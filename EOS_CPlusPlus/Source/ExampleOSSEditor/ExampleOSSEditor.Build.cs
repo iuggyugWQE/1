83702f49fd95c84ff1a3f46c5ce150293775cb1c
June Rhodes <jrhodes@redpoint.games>
2023-02-14 14:09:55 +1100

    Update projects for new EOS release

--- EOS_CPlusPlus/Source/ExampleOSSEditor/ExampleOSSEditor.Build.cs
+++ EOS_CPlusPlus/Source/ExampleOSSEditor/ExampleOSSEditor.Build.cs
@@ -9,24 +9,24 @@ public class ExampleOSSEditor : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 #if UE_5_1_OR_LATER
-        PublicDefinitions.Add("UE_5_1_OR_LATER=1");
+        PrivateDefinitions.Add("UE_5_1_OR_LATER=1");
 #endif
 #if UE_5_0_OR_LATER
-        PublicDefinitions.Add("UE_5_0_OR_LATER=1");
+        PrivateDefinitions.Add("UE_5_0_OR_LATER=1");
 #endif
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystemUtils" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { 
-            "OnlineSubsystem", 
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            "OnlineSubsystem",
             "ExampleOSSEarlyConfig",
 #if UE_5_0_OR_LATER
             "HTTP",
 #else
             "Http",
 #endif
-            "Json", 
-            "AssetRegistry", 
+            "Json",
+            "AssetRegistry",
             "OnlineSubsystemRedpointEOS",
             "BlueprintGraph",
             "UnrealEd",
